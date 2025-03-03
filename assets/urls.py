a9d1de7c0bab068622372e054bd5e7baa562d527
KudakwasheMurungweni <kudakwashemurungweni50@gmail.com>
2025-03-03 16:21:23 +0200

    Templates done now lets fix to see maintanance log and cost report

--- assets/urls.py
+++ assets/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import home, asset_list, maintenance_report, cost_report, add_asset
 
 urlpatterns = [
-    path('', home, name='home'),  # Add homepage route
+    path('', home, name='home'),  # Root URL points to the home page
     path('assets/', asset_list, name='asset_list'),
     path('maintenance-report/', maintenance_report, name='maintenance_report'),
     path('cost-report/', cost_report, name='cost_report'),
-    path('add-asset/', add_asset, name='add_asset'), 
+    path('add-asset/', add_asset, name='add_asset'),
 ]
